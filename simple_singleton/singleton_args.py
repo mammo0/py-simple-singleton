f892ef53667b2927ac279af91b3ec520a5f113b7
Tony Diana <info@tonydiana.es>
2021-05-29 22:47:34 -0600

    Update singleton_args.py
    GIL control threading

--- simple_singleton/singleton_args.py
+++ simple_singleton/singleton_args.py
@@ -6,35 +6,46 @@ thanks to wowkin2 (https://gist.github.com/wowkin2/3af15bfbf197a14a2b0b2488a1e8c
 
 import inspect
 
+import socket
+from threading import Lock
 
-class _SingletonArgsMeta(type):
+
+# --- Controlador de bloqueos
+GIL: Lock = Lock()
+
     """
     Singleton that keep single instance for single set of arguments.
+    https://github.com/mammo0/py-simple-singleton
     """
     _instances = {}
     _init = {}
 
     def __init__(cls, name: str, bases: tuple, dct: dict):
-        super(_SingletonArgsMeta, cls).__init__(name, bases, dct)
+        super(SingletonArgsMeta, cls).__init__(name, bases, dct)
 
         # save the __init__ method of each class that uses this singleton
         # required in __call__ method below
         cls._init[cls] = getattr(cls, "__init__")
 
     def __call__(cls, *args, **kwargs):
-        # get the individual calling signature of the __init__ method for this class
-        init_callargs = inspect.getcallargs(cls._init[cls], None, *args, **kwargs)
 
-        # create a key
-        key = hash(
-            # the individual key is this class combined with the signature above
-            (cls, cls.__freeze_dict(init_callargs))
-        )
+        # --- Sólo si GIL le da paso
+        with GIL:
+            # get the individual calling signature of the __init__ method for
+            # this class
+            init_callargs = inspect.getcallargs(cls._init[cls],
+                                                None, *args, **kwargs)
 
-        # check if there's already an instance that has the same __init__ signature
-        if key not in cls._instances:
-            # if not, create it
-            cls._instances[key] = super(_SingletonArgsMeta, cls).__call__(*args, **kwargs)
+            # create a key
+            key = hash((cls, cls.__freeze_dict(init_callargs)))
+            # the individual key is this class combined with the signature
+            # above
+
+            # check if there's already an instance that has the same __init__
+            # signature
+            if key not in cls._instances:
+                # if not, create it
+                cls._instances[key] = super(SingletonArgsMeta, cls).__call__(*args, **kwargs)
 
         return cls._instances[key]
 
