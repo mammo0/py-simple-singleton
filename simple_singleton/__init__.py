f9f0235da1a1ef305f50449b426dc9f14b514026
mammo0 <marc.ammon@hotmail.de>
2021-06-01 15:13:23 +0200

    added thread-safe singleton meta classes

--- simple_singleton/__init__.py
+++ simple_singleton/__init__.py
@@ -1,9 +1,13 @@
 from simple_singleton.singleton import _SingletonMeta
 from simple_singleton.singleton_args import _SingletonArgsMeta
+from simple_singleton.thread_singleton import _ThreadSingletonMeta, \
+    _ThreadSingletonArgsMeta
 
 
-__all__ = ["Singleton", "SingletonArgs"]
+__all__ = ["Singleton", "ThreadSingleton", "SingletonArgs", "ThreadSingletonArgs"]
 
 
 Singleton = _SingletonMeta
+ThreadSingleton = _ThreadSingletonMeta
 SingletonArgs = _SingletonArgsMeta
+ThreadSingletonArgs = _ThreadSingletonArgsMeta
---
+++ simple_singleton/thread_singleton.py
@@ -0,0 +1,29 @@
+"""
+Extension for the singleton pattern to make it thread safe.
+thanks to TonyDiana for the idea (https://github.com/mammo0/py-simple-singleton/pull/5)
+"""
+from threading import Lock
+
+from simple_singleton.singleton import _SingletonMeta
+from simple_singleton.singleton_args import _SingletonArgsMeta
+
+
+class _ThreadSafeMixin(type):
+    def __init__(cls, name: str, bases: tuple, dct: dict):
+        super(_ThreadSafeMixin, cls).__init__(name, bases, dct)
+
+        # add a lock for thread safety
+        cls._lock: Lock = Lock()
+
+    def __call__(cls, *args, **kwargs):
+        # use the lock
+        with cls._lock:
+            return super(_ThreadSafeMixin, cls).__call__(*args, **kwargs)
+
+
+class _ThreadSingletonMeta(_ThreadSafeMixin, _SingletonMeta):
+    pass
+
+
+class _ThreadSingletonArgsMeta(_ThreadSafeMixin, _SingletonArgsMeta):
+    pass
